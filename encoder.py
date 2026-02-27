c9f2cccd39b7b19c2ef50e71653ee9132eeaf303
Ahmed Abbas <abbasa29@mcmaster.ca>
2026-02-27 17:17:05 -0500

    working on encoder

--- encoder.py
+++ encoder.py
@@ -2,7 +2,8 @@ from torch import nn
 from multihead_attention import MultiheadAttention
 from positional_encoding import PositionalEncoding
 
-class EncoderBlock(nn.Module):
+# General transformer block: contains the multihead attention and feed forward network
+class TransformerBlock(nn.Module):
     def __init__(
         self,
         embedding_dim = 512,
@@ -10,5 +11,49 @@ class EncoderBlock(nn.Module):
         dropout = 0.1,
         forward_expansion = 4
     ):
-        super(EncoderBlock, self).__init__()
+        super(TransformerBlock, self).__init__()
+        self.attention = MultiheadAttention(embedding_dim, heads)
+        self.norm1 = nn.LayerNorm(embedding_dim)
+        self.feed_forward = nn.Sequential(
+            nn.Linear(embedding_dim, forward_expansion * embedding_dim),
+            nn.ReLU(),
+            nn.Linear(forward_expansion * embedding_dim, embedding_dim)
+        )
+        self.norm2 = nn.LayerNorm(embedding_dim)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, queries, keys, values, mask=None):
+        attention = self.attention(queries, keys, values, mask)
+        # Add skip connection and attention output
+        x = self.dropout(self.norm1(attention + queries))
+        # Feed forward network
+        forward = self.feed_forward(x)
+        # Second normalization and dropout
+        x = self.dropout(self.norm2(forward + x))
+        return x
+
+
+class Encoder(nn.Module):
+    def __init__(
+        self,
+        src_vocab_size,
+        device,
+        embedding_dim = 512,
+        num_layers = 6,
+        heads = 8,
+        forward_expansion = 4,
+        dropout = 0.1,
+        max_length = 100
+    ):
+        super(Encoder, self).__init__()
+        self.embedding_dim = embedding_dim
+        self.device = device
+        self.word_embedding = nn.Embedding(src_vocab_size, embedding_dim)
+        self.positional_embedding = nn.Embedding(max_length, embedding_dim)
+        self.layers = nn.ModuleList([TransformerBlock(embedding_dim, heads, forward_expansion, dropout)])
+        self.dropout = nn.Dropout(dropout)
+    
+    def forward(self, x, mask=None):
+        # N examples, seq_length tokens
+        N, seq_length = x.shape
         
